7ce46b82aeca9f9006f1a76115db954d128f47ab
Mario Lazzari <mario.lazzari@gmail.com>
2024-11-24 18:24:03 +0100

    feat: open days

--- app/brochures/page.tsx
+++ app/brochures/page.tsx
@@ -53,20 +53,58 @@ function BrochuresPage() {
       imagePath: "halloween3.png",
       href: "halloween.pdf",
     },
+    {
+      title: "Halloween VS Lo dias de los muertos",
+      subtitle: "Quarto episodio",
+      description: "Halloween in spagnolo",
+      imagePath: "halloween4.png",
+      href: "halloweenSpa.pdf",
+    },
+  ];
+
+  const events: BrochureProps[] = [
+    {
+      title: "Open day",
+      subtitle: "Mercoledì 11 settembre 2024 ore 18:00",
+      description: "Via Trieste 77L, Montichiari",
+      imagePath: "openday1.png",
+      href: "openday1.png",
+    },
+    {
+      title: "Open day",
+      subtitle: "Mercoledì 11 settembre 2024 ore 18:00",
+      description: "Via Trieste 77L, Montichiari",
+      imagePath: "openday2.png",
+      href: "openday2.png",
+    },
+    {
+      title: "Serata fumetto",
+      subtitle: "Con il fumettista Marco Galli",
+      description: "Il manga giapponese incotra il fumetto occidentale",
+      imagePath: "comics.png",
+      href: "comics.pdf",
+    },
   ];
 
   return (
     <div className="flex flex-col items-center">
-      <h2 className="text-4xl text-center my-8">Conversazioni</h2>
-      <div className="flex justify-center items-center flex-wrap gap-8">
+      <h2 className="text-4xl text-center my-12">Conversazioni</h2>
+      <div className="flex justify-center items-center flex-wrap gap-16">
         {conversations.map(conv => (
           <Brochure key={conv.title} {...conv} />
         ))}
       </div>
 
-      <h2 className="text-4xl text-center my-8">Halloween</h2>
-      <div className="flex justify-center items-center flex-wrap gap-8">
+      <h2 className="text-4xl text-center my-12">Halloween</h2>
+      <div className="flex justify-center items-center flex-wrap gap-16">
         {hallos.map(conv => (
+          <Brochure key={conv.title} {...conv} />
+        ))}
+      </div>
+
+      <h2 className="text-4xl text-center my-12">Eventi</h2>
+      <div className="flex justify-center items-center flex-wrap gap-16">
+        {events.map(conv => (
           <Brochure key={conv.title} {...conv} />
         ))}
       </div>
