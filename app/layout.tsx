c04f3e397b1585896359b5ff626d7a580f7f2bc0
Mario Lazzari <mario.lazzari@gmail.com>
2025-01-13 22:48:55 +0100

    refact: facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,6 @@ import { ThemeProvider } from "next-themes";
 import { Footer } from "@/components/Footer";
 import { Layout } from "@/types/Layout";
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
-import Script from "next/script";
-import Image from "next/image";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://gfmontiform.it"),
@@ -37,7 +35,6 @@ function RootLayout({ children }: Layout) {
       <head>
         <GoogleTagManager gtmId="GTM-T3KQ5P9J" />
         <GoogleAnalytics gaId="G-M2676NK92Z" />
-        <Script src="/scripts/pixel.js" strategy="beforeInteractive" />
       </head>
 
       <body
@@ -50,15 +47,6 @@ function RootLayout({ children }: Layout) {
           </div>
           <Footer />
         </ThemeProvider>
-
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=975906963878222&ev=PageView&noscript=1"
-          />
-        </noscript>
       </body>
     </html>
   );
