dbe4e3242e7bd61ed2db70a267f3e42ee74ac156
Mario Lazzari <mario.lazzari@gmail.com>
2025-01-15 20:48:18 +0100

    refact: brochure page

--- app/brochures/page.tsx
+++ app/brochures/page.tsx
@@ -1,5 +1,12 @@
 import Brochure from "@/components/Brochure";
-import { BrochureProps } from "@/components/Brochure/BrochureProps";
+import {
+  cits,
+  conversations,
+  cult,
+  events,
+  fridays,
+  hallos,
+} from "@/db/brochures";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -10,147 +17,6 @@ export const metadata: Metadata = {
 };
 
 function BrochuresPage() {
-  const conversations: BrochureProps[] = [
-    {
-      title: "English conversation",
-      subtitle: "Conversazione sabato mattina",
-      description: "Migliora il tuo inglese in un'atmosfera rilassata",
-      imagePath: "saturday.png",
-      href: "saturday.pdf",
-    },
-    {
-      title: "Conversasione spagnola",
-      subtitle: "Ogni martedì sera",
-      description: "Dalle ore 18:30 alle ore 20:00",
-      imagePath: "spanish.png",
-      href: "spanish.pdf",
-    },
-    {
-      title: "Nuova lingua",
-      subtitle: "Hai voglia di imparare una nuva lingua",
-      description: "Le meraviglie di una nuova lingua",
-      imagePath: "new.png",
-      href: "new.pdf",
-    },
-  ];
-
-  const hallos: BrochureProps[] = [
-    {
-      title: "Halloween vs Ognissanti",
-      subtitle: "Primo episodio",
-      description: "Significati diversi",
-      imagePath: "hallo1.png",
-      href: "hallo.pdf",
-    },
-    {
-      title: "Halloween vs Ognissanti",
-      subtitle: "Secondo episodio",
-      description: "Origine anglosassone",
-      imagePath: "hallo2.png",
-      href: "hallo.pdf",
-    },
-    {
-      title: "Halloween vs Ognissanti",
-      subtitle: "Terzo episodio",
-      description: "Radici cattoliche",
-      imagePath: "hallo3.png",
-      href: "hallo.pdf",
-    },
-    {
-      title: "Halloween VS Lo dias de los muertos",
-      subtitle: "Quarto episodio",
-      description: "Halloween in spagnolo",
-      imagePath: "hallo4.png",
-      href: "hallo_spa.pdf",
-    },
-  ];
-
-  const events: BrochureProps[] = [
-    {
-      title: "Open day",
-      subtitle: "Mercoledì 11 settembre 2024 ore 18:00",
-      description: "Via Trieste 77L, Montichiari",
-      imagePath: "openday1.png",
-      href: "openday1.png",
-    },
-    {
-      title: "Open day",
-      subtitle: "Mercoledì 11 settembre 2024 ore 18:00",
-      description: "Via Trieste 77L, Montichiari",
-      imagePath: "openday2.png",
-      href: "openday2.png",
-    },
-    {
-      title: "Serata fumetto",
-      subtitle: "Con il fumettista Marco Galli",
-      description: "Il manga giapponese incotra il fumetto occidentale",
-      imagePath: "comics.png",
-      href: "comics.pdf",
-    },
-  ];
-
-  const cits: BrochureProps[] = [
-    {
-      title: "Citazione motivazionale",
-      subtitle: "Prima citazione",
-      description: "Prima citazione motivazionale",
-      imagePath: "cit1.png",
-      href: "cit1.png",
-    },
-    {
-      title: "Citazione motivazionale",
-      subtitle: "Seconda citazione",
-      description: "Seconda citazione motivazionale",
-      imagePath: "cit2.png",
-      href: "cit2.png",
-    },
-    {
-      title: "Citazione motivazionale",
-      subtitle: "Terza citazione",
-      description: "Terza citazione motivazionale",
-      imagePath: "cit3.png",
-      href: "cit3.png",
-    },
-    {
-      title: "Citazione motivazionale",
-      subtitle: "Quarta citazione",
-      description: "Quarta citazione motivazionale",
-      imagePath: "cit4.png",
-      href: "cit4.png",
-    },
-  ];
-
-  const fridays: BrochureProps[] = [
-    {
-      title: "Venerdì",
-      subtitle: "Prima citazione",
-      description: "Prima citazione motivazionale",
-      imagePath: "friday1.png",
-      href: "friday.pdf",
-    },
-    {
-      title: "Venerdì",
-      subtitle: "Seconda citazione",
-      description: "Seconda citazione motivazionale",
-      imagePath: "friday2.png",
-      href: "friday.pdf",
-    },
-    {
-      title: "Venerdì",
-      subtitle: "Terza citazione",
-      description: "Terza citazione motivazionale",
-      imagePath: "friday3.png",
-      href: "friday.pdf",
-    },
-    {
-      title: "Venerdì",
-      subtitle: "Quarta citazione",
-      description: "Quarta citazione motivazionale",
-      imagePath: "friday4.png",
-      href: "friday.pdf",
-    },
-  ];
-
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-4xl text-center my-12">Conversazioni</h2>
@@ -163,21 +29,21 @@ function BrochuresPage() {
       <h2 className="text-4xl text-center my-12">Halloween</h2>
       <div className="flex justify-center items-center flex-wrap gap-16">
         {hallos.map(conv => (
-          <Brochure key={conv.title} {...conv} />
+          <Brochure key={conv.imagePath} {...conv} />
         ))}
       </div>
 
       <h2 className="text-4xl text-center my-12">Eventi</h2>
       <div className="flex justify-center items-center flex-wrap gap-16">
         {events.map(conv => (
-          <Brochure key={conv.title} {...conv} />
+          <Brochure key={conv.imagePath} {...conv} />
         ))}
       </div>
 
       <h2 className="text-4xl text-center my-12">Citazioni motivazionali</h2>
       <div className="flex justify-center items-center flex-wrap gap-16">
         {cits.map(conv => (
-          <Brochure key={conv.href} {...conv} />
+          <Brochure key={conv.imagePath} {...conv} />
         ))}
       </div>
 
@@ -185,8 +51,15 @@ function BrochuresPage() {
         Citazioni motivazionali per il venerdì
       </h2>
       <div className="flex justify-center items-center flex-wrap gap-16">
-        {fridays.map(conv => (
-          <Brochure key={conv.subtitle} {...conv} />
+        {fridays.map(f => (
+          <Brochure key={f.imagePath} {...f} />
+        ))}
+      </div>
+
+      <h2 className="text-4xl text-center my-12">Cultura</h2>
+      <div className="flex justify-center items-center flex-wrap gap-16">
+        {cult.map(c => (
+          <Brochure key={c.imagePath} {...c} />
         ))}
       </div>
     </div>
---
+++ db/brochures.ts
@@ -0,0 +1,159 @@
+import { BrochureProps } from "@/components/Brochure/BrochureProps";
+
+export const conversations: BrochureProps[] = [
+  {
+    title: "English conversation",
+    subtitle: "Conversazione sabato mattina",
+    description: "Migliora il tuo inglese in un'atmosfera rilassata",
+    imagePath: "saturday.png",
+    href: "saturday.pdf",
+  },
+  {
+    title: "Conversasione spagnola",
+    subtitle: "Ogni martedì sera",
+    description: "Dalle ore 18:30 alle ore 20:00",
+    imagePath: "spanish.png",
+    href: "spanish.pdf",
+  },
+  {
+    title: "Nuova lingua",
+    subtitle: "Hai voglia di imparare una nuva lingua",
+    description: "Le meraviglie di una nuova lingua",
+    imagePath: "new.png",
+    href: "new.pdf",
+  },
+];
+
+export const hallos: BrochureProps[] = [
+  {
+    title: "Halloween vs Ognissanti",
+    subtitle: "Primo episodio",
+    description: "Significati diversi",
+    imagePath: "hallo1.png",
+    href: "hallo.pdf",
+  },
+  {
+    title: "Halloween vs Ognissanti",
+    subtitle: "Secondo episodio",
+    description: "Origine anglosassone",
+    imagePath: "hallo2.png",
+    href: "hallo.pdf",
+  },
+  {
+    title: "Halloween vs Ognissanti",
+    subtitle: "Terzo episodio",
+    description: "Radici cattoliche",
+    imagePath: "hallo3.png",
+    href: "hallo.pdf",
+  },
+  {
+    title: "Halloween VS Lo dias de los muertos",
+    subtitle: "Quarto episodio",
+    description: "Halloween in spagnolo",
+    imagePath: "hallo4.png",
+    href: "hallo_spa.pdf",
+  },
+];
+
+export const events: BrochureProps[] = [
+  {
+    title: "Open day",
+    subtitle: "Mercoledì 11 settembre 2024 ore 18:00",
+    description: "Via Trieste 77L, Montichiari",
+    imagePath: "openday1.png",
+    href: "openday1.png",
+  },
+  {
+    title: "Open day",
+    subtitle: "Mercoledì 11 settembre 2024 ore 18:00",
+    description: "Via Trieste 77L, Montichiari",
+    imagePath: "openday2.png",
+    href: "openday2.png",
+  },
+  {
+    title: "Serata fumetto",
+    subtitle: "Con il fumettista Marco Galli",
+    description: "Il manga giapponese incotra il fumetto occidentale",
+    imagePath: "comics.png",
+    href: "comics.pdf",
+  },
+];
+
+export const cits: BrochureProps[] = [
+  {
+    title: "Citazione motivazionale",
+    subtitle: "Prima citazione",
+    description: "Prima citazione motivazionale",
+    imagePath: "cit1.png",
+    href: "cit1.png",
+  },
+  {
+    title: "Citazione motivazionale",
+    subtitle: "Seconda citazione",
+    description: "Seconda citazione motivazionale",
+    imagePath: "cit2.png",
+    href: "cit2.png",
+  },
+  {
+    title: "Citazione motivazionale",
+    subtitle: "Terza citazione",
+    description: "Terza citazione motivazionale",
+    imagePath: "cit3.png",
+    href: "cit3.png",
+  },
+  {
+    title: "Citazione motivazionale",
+    subtitle: "Quarta citazione",
+    description: "Quarta citazione motivazionale",
+    imagePath: "cit4.png",
+    href: "cit4.png",
+  },
+];
+
+export const fridays: BrochureProps[] = [
+  {
+    title: "Venerdì",
+    subtitle: "Prima citazione",
+    description: "Prima citazione motivazionale",
+    imagePath: "friday1.png",
+    href: "friday.pdf",
+  },
+  {
+    title: "Venerdì",
+    subtitle: "Seconda citazione",
+    description: "Seconda citazione motivazionale",
+    imagePath: "friday2.png",
+    href: "friday.pdf",
+  },
+  {
+    title: "Venerdì",
+    subtitle: "Terza citazione",
+    description: "Terza citazione motivazionale",
+    imagePath: "friday3.png",
+    href: "friday.pdf",
+  },
+  {
+    title: "Venerdì",
+    subtitle: "Quarta citazione",
+    description: "Quarta citazione motivazionale",
+    imagePath: "friday4.png",
+    href: "friday.pdf",
+  },
+];
+
+export const cult: BrochureProps[] = [
+  {
+    title: "Carte cultura",
+    subtitle: "Possiedi una carta della cultura?",
+    description: "Spendi qui le tue carte cultura e del merito",
+    imagePath: "cult.png",
+    href: "cult.pdf",
+  },
+  {
+    title: "Imparare 2.0",
+    subtitle: "Lingue e tecnologie a confronto",
+    description: "Scopri il potenziale della tecnologia per imparare le lingue",
+    imagePath: "pot.png",
+    href: "pot.pdf",
+  },
+];
