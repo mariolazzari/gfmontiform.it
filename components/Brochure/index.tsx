67743b3aad9f7ee4b5a8a31b94aed3b74467e6ca
Mario Lazzari <mario.lazzari@gmail.com>
2025-04-20 08:20:36 +0200

    fix: brochure care sizes

--- components/Brochure/index.tsx
+++ components/Brochure/index.tsx
@@ -26,14 +26,16 @@ function Brochure({
           <CardDescription>{subtitle}</CardDescription>
         </CardHeader>
 
-        <CardContent className="w-[350px]">
-          <Image
-            src={`/images/brochures/${imagePath}`}
-            alt={title}
-            width={350}
-            height={250}
-            priority
-          />
+        <CardContent className="w-[350px] h-[300px]">
+          <div className="relative w-[300px] h-[250px] overflow-y-auto">
+            <Image
+              src={`/images/brochures/${imagePath}`}
+              alt={title}
+              width={350}
+              height={250}
+              priority
+            />
+          </div>
           <p className="text-justify text-sm my-4">{description}</p>
         </CardContent>
 
